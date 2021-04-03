c08ca72807dd858399bdc9bf5d71ad10a6d38d7c
Zakaria-Ahmed2021 <81860033+Zakaria-Ahmed2021@users.noreply.github.com>
2021-04-03 18:03:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
+	var p,t,r,si;
     p = document.getElementById("principal").value;
-    
+    t = document.getElementById ("years").value;
+	r = document.getElementById ("rate").value;
+	si = parseInt((p*t*r)/100 );
+
+	let date =  new Date().getFullYear();
+	console.log(date);
+	y=(+date)+(+t);
+	document.getElementById ('num0').innerHTML ="If you deposit "+p;
+	document.getElementById ('num2').innerHTML ="at an interest rate of "+r+"%";
+	document.getElementById ('num').innerHTML ="You will receive an amount of "+si;
+	document.getElementById ('num1').innerHTML ="in the year "+y;
 }
-        
